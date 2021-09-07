0273bec52062f1118f87d3fe4a54a79b43740022
Maycon <mayconprata90@gmail.com>
2021-09-07 09:09:27 -0300

    ajustando cors

--- src/main/java/com/desenvolvimento/aplicacoes/corp20212/CryptoGO/core/security/token/RefreshTokenPostProcessor.java
+++ src/main/java/com/desenvolvimento/aplicacoes/corp20212/CryptoGO/core/security/token/RefreshTokenPostProcessor.java
@@ -47,7 +47,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 		private void adicionarRefreshTokeNoCookie(String refreshToken, HttpServletRequest req, HttpServletResponse res) {
 			Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
 			refreshTokenCookie.setHttpOnly(true);
-			refreshTokenCookie.setSecure(false);
+			refreshTokenCookie.setSecure(true);
 			refreshTokenCookie.setPath(req.getContextPath() + "/oauth/token");
 			refreshTokenCookie.setMaxAge(2592000);
 			res.addCookie(refreshTokenCookie);
